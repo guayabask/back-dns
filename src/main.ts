2e7bd46af03f357dae8955907a0bb36f022b514c
guayabask <wua.carlos.14@gmail.com>
2025-07-05 23:12:27 -0500

    Rail

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Habilitar CORS para permitir peticiones desde el frontend
+
   app.enableCors({
-    origin: 'http://localhost:5173', // cambia si tu frontend está en otro dominio o puerto
-  });
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+});
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
